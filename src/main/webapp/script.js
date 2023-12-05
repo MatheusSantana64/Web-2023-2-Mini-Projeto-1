df3a58a0977b2b021850fda536de5660d82a0dad
mutannejs <murillojustinoklein@gmail.com>
2023-12-04 21:49:16 -0300

    cards iniciado

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,12 +1,3 @@
-/* function loadDoc() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        myFunction(this);
-    }
-    xhttp.open("GET", "itinerarios_onibus.xml");
-    xhttp.send();
-} */
-
 window.onload = function()
 {
     const xhttp = new XMLHttpRequest();
@@ -20,17 +11,18 @@ window.onload = function()
 function myFunction(xml) {
     const xmlDoc = xml.responseXML;
     const x = xmlDoc.getElementsByTagName("ONIBUS");
-    let table="<tr><th>Onibus</th><th>Numero</th><th>Partida</th><th>Destino</th></tr>";
-    for (let i = 0; i < x.length; i++) {
-        table += "<tr><td>" +
-        x[i].getElementsByTagName("NOME")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-        x[i].getElementsByTagName("NUMERO")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-        x[i].getElementsByTagName("PARTIDA")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-        x[i].getElementsByTagName("DESTINO")[0].childNodes[0].nodeValue +
-        "</td></tr>";
+    let cards = "";
+    for (let i = 0; i < 8; i++) {
+        console.log(i)
+        cards += `<div class="card"><h1>` +
+                x[i].getElementsByTagName("NOME")[0].childNodes[0].nodeValue +
+                "</h1><p>Numero: " +
+                x[i].getElementsByTagName("NUMERO")[0].childNodes[0].nodeValue +
+                "</p><p>Partida: " +
+                x[i].getElementsByTagName("PARTIDA")[0].childNodes[0].nodeValue +
+                "</p><p>Destino: " +
+                x[i].getElementsByTagName("DESTINO")[0].childNodes[0].nodeValue +
+                `</p><p><button type="button" action="visualizar()"></p></div>`;
     }
-    document.getElementById("cards").innerHTML = table;
+    document.getElementById("container_cards").innerHTML = cards;
 }
