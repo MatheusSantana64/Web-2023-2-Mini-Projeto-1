293646ebb909400b9eafb6a507cfebdaca4cce0b
Matheus Cerqueira Santana <matheus.santana6415@gmail.com>
2023-12-08 12:37:38 -0300

    Pequenos ajustes no Cadastro
    pontosporregiao.xml renomeado para pontos_regiao.xml;
    Definidos para UTF-8 a página itinerarios_cadastro.html e a lista de pontos montada no CadastroItinerarioServlet.java;
    datalist (Sugestões de preenchimento) para o campo de Partida no Cadastro.

--- src/main/java/CadastroIntinerarioServlet.java
+++ src/main/java/CadastroIntinerarioServlet.java
@@ -20,34 +20,32 @@ public class CadastroIntinerarioServlet extends HttpServlet {
         String busreginame = request.getParameter("busreginame");
         try {
             // Lê o arquivo XML e armazena no doc
-            Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(getServletContext().getRealPath("/pontosporregiao.xml"));
+            Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(getServletContext().getRealPath("/pontos_regiao.xml"));
             // Obtém todos os ônibus do arquivo XML
             NodeList regiaoList = doc.getElementsByTagName(busreginame);
+            // Define o tipo de conteúdo da resposta e a codificação de caracteres
+            response.setContentType("text/html; charset=UTF-8");
             // Obtém o escritor da resposta
             PrintWriter out = response.getWriter();
-            // Itera sobre cada ônibus
-          
-                // Obtém o elemento do ônibus atual (Converte o nó em elemento)
-                Element regiaoElement = (Element) regiaoList.item(0);
-             
-                    // Obtém todos os pontos de parada do ônibus
-                    NodeList stopList = regiaoElement.getElementsByTagName("PARADAS").item(0).getChildNodes();
-                    // Define a codificação de caracteres da resposta
-                    response.setCharacterEncoding("UTF-8");
-                    
-                    // Itera sobre cada ponto de parada
-                    for (int j = 0; j < stopList.getLength(); j++) {
-                        // Obtém o nó do ponto de parada atual
-                        Node stop = stopList.item(j);
-                        // Verifica se o nó é um elemento
-                        if (stop.getNodeType() == Node.ELEMENT_NODE) {
-                            // Escreve o ponto de parada na resposta
-                            out.println(((Element) stop).getTextContent());
-                        }
-                    }
-                    
-                
+
+            // Obtém o elemento do ônibus atual (Converte o nó em elemento)
+            Element regiaoElement = (Element) regiaoList.item(0);
+
+            // Obtém todos os pontos de parada do ônibus
+            NodeList stopList = regiaoElement.getElementsByTagName("PARADAS").item(0).getChildNodes();
+            // Define a codificação de caracteres da resposta
+            response.setCharacterEncoding("UTF-8");
             
+            // Itera sobre cada ponto de parada
+            for (int j = 0; j < stopList.getLength(); j++) {
+                // Obtém o nó do ponto de parada atual
+                Node stop = stopList.item(j);
+                // Verifica se o nó é um elemento
+                if (stop.getNodeType() == Node.ELEMENT_NODE) {
+                    // Escreve o ponto de parada na resposta
+                    out.println(((Element) stop).getTextContent());
+                }
+            }
         } catch (Exception e) {
             // Imprime a pilha de chamadas da exceção
             e.printStackTrace();
