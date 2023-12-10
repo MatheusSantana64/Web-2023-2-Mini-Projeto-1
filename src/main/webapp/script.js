9a857f08b87c9d8e3b85c6ea7d91abbdc7e80fa6
mutannejs <murillojustinoklein@gmail.com>
2023-12-10 13:12:14 -0300

    tarifa retirada do xml

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -10,19 +10,19 @@ window.onload = function()
 
 function myFunction(xml) {
     const xmlDoc = xml.responseXML;
-    const x = xmlDoc.getElementsByTagName("ONIBUS");
+    const onibus = xmlDoc.getElementsByTagName("ONIBUS");
     let cards = "";
-    for (let i = 0; i < 8 && i < x.length; i++) {
+    for (let i = 0; i < 8 && i < onibus.length; i++) {
         cards += `<div class="card"><h1>` +
-                x[i].getElementsByTagName("NOME")[0].childNodes[0].nodeValue +
+                onibus[i].getElementsByTagName("NOME")[0].childNodes[0].nodeValue +
                 "</h1><p>Numero: " +
-                x[i].getElementsByTagName("NUMERO")[0].childNodes[0].nodeValue +
+                onibus[i].getElementsByTagName("NUMERO")[0].childNodes[0].nodeValue +
                 "</p><p>Partida: " +
-                x[i].getElementsByTagName("PARTIDA")[0].childNodes[0].nodeValue +
+                onibus[i].getElementsByTagName("PARTIDA")[0].childNodes[0].nodeValue +
                 "</p><p>Destino: " +
-                x[i].getElementsByTagName("DESTINO")[0].childNodes[0].nodeValue +
+                onibus[i].getElementsByTagName("DESTINO")[0].childNodes[0].nodeValue +
                 `</p><p class="func">` +
-                x[i].getElementsByTagName("FUNCIONAMENTO")[0].childNodes[0].nodeValue +
+                onibus[i].getElementsByTagName("FUNCIONAMENTO")[0].childNodes[0].nodeValue +
                 `</p></div>`;
     }
     document.getElementById("container_cards").innerHTML = cards;
